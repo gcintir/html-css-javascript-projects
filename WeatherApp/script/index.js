5b230eaaba94770f0d821b082e864c3d2433be5d
DIR\guray.cintir <guray.cintir@accenture.com>
2024-08-29 23:34:43 +0300

    query current weather condition by location from Weather API service

--- WeatherApp/script/index.js
+++ WeatherApp/script/index.js
@@ -1,29 +1,108 @@
-
-import { getWeatherCondition } from './weather-condition-api.js';
+import { getWeatherCondition } from "./weather-condition-api.js";
 
 init();
 
-function init () {
-    document.querySelector('.search-button').addEventListener('click', () => {
-        displayWeatherConditionResultDivision();
-    });
+function init() {
+  document.querySelector(".js-search-button").addEventListener("click", () => {
+    handleWeatherConditionQuery();
+  });
 
+  document.querySelector('.js-location-input').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        handleWeatherConditionQuery();
+    }
+  });
 }
 
-function displayWeatherConditionResultDivision () {
-    document.querySelector('.wc-data-container').classList.toggle('hidden');
-    document.querySelector('.wc-forecast-container').classList.toggle('hidden');
+async function handleWeatherConditionQuery() {
+  const location = getLocationInput();
+  const weatherCondition = await getWeatherCondition(location);
+  renderCurrentWeatherConditionView(
+    weatherCondition.getLocation(),
+    weatherCondition.getCurrentWeatherCondition()
+  );
+  displayWeatherConditionView();
 }
 
-async function displayWeatherCondition () {
-    const city = document.querySelector('.city-input').value;
-    const weatherCondition = await getWeatherCondition(city);
-    //console.log(JSON.stringify(weatherCondition.getLocation()));
-    renderWeatherCondition(weatherCondition);
+function renderCurrentWeatherConditionView(location, currentWeatherCondition) {
+  let currentWeatherConditionHTML = `
+    <div>
+                <span>Last updated at ${currentWeatherCondition.lastUpdatedAt}</span>
+            </div>
+            <div class="wc-temperature-container">
+                <div>
+                    <img class="wc-temperature-icon" src="icons/sunny.webp" alt="">
+                </div>
+                <div>
+                    <p class="temperature-info">${currentWeatherCondition.tempC} &deg;C</p>
+                    <p class="temperature-feel-info">Feels like ${currentWeatherCondition.feelslikeC} &deg;C</p>
+                    <p class="wc-info">${currentWeatherCondition.text}</p>
+                </div>
+            </div>
+            <div class="location-info">
+                <p>${location.name}, ${location.region}, ${location.country}</p>
+            </div>
+            <div class="location-datetime-info">
+                ${location.localtime}
+            </div>
+            <div class="wc-info-container">
+                <div class="wc-humidity-container">
+                    <div>
+                        <img class="humidity-icon" src="icons/humidity.png" alt="">
+                        <p>Humidity</p>
+                    </div>
+                    <div>
+                        <p class="wc-humidity-info">${currentWeatherCondition.humidity}%</p>
+                    </div>
+                </div>
+                <div class="wc-airq-container">
+                    <div>
+                        <img class="airq-icon" src="icons/air_quality.png" alt="">
+                        <p>Air Quality</p>
+                    </div>
+                    <div>
+                        <p class="airq-info">co: ${currentWeatherCondition.co}</p>
+                        <p class="airq-info">no2: ${currentWeatherCondition.no2}</p>
+                        <p class="airq-info">o3: ${currentWeatherCondition.o3}</p>
+                        <p class="airq-info">so2: ${currentWeatherCondition.so2}</p>
+                        <p class="airq-info">${currentWeatherCondition.usEpaIndex}</p>
+                    </div>
+                </div>
+                <div class="wc-wind-container">
+                    <div>
+                        <img class="wind-icon" src="icons/wind_power.png" alt="">
+                        <p>Wind Power</p>
+                    </div>
+                    <div>
+                        <p class="wind-info">${currentWeatherCondition.windKph} km/h</p>
+                        <p class="wind-info">${currentWeatherCondition.windDir}</p>
+                    </div>
+                </div>
+            </div>
+    `;
+  document.querySelector(".js-wc-data-container").innerHTML =
+    currentWeatherConditionHTML;
 }
 
-function renderWeatherCondition (weatherCondition) {
-    document.querySelector('.weather-condition-text').innerText = weatherCondition.getCurrent()['condition']['text'];
-    document.querySelector('.weather-condition-image').src = weatherCondition.getCurrent()['condition']['icon'];
-    document.querySelector('.weather-condition-temperature').innerText = weatherCondition.getCurrent()['temp_c'] + '°C';
+function getLocationInput() {
+  const location = document.querySelector(".js-location-input").value;
+  return location;
+}
+
+function displayWeatherConditionView() {
+  if (
+    document.querySelector(".js-wc-data-container").classList.contains("hidden")
+  ) {
+    document.querySelector(".js-wc-data-container").classList.remove("hidden");
+  }
+
+  if (
+    document
+      .querySelector(".js-wc-forecast-container")
+      .classList.contains("hidden")
+  ) {
+    document
+      .querySelector(".js-wc-forecast-container")
+      .classList.remove("hidden");
+  }
 }
--- WeatherApp/script/weather-condition.js
+++ WeatherApp/script/weather-condition.js
@@ -1,13 +1,49 @@
 export class WeatherCondition {
     #location;
-    #current;
-    #forecast;
+    #currentWeatherCondition;
+    #forecastWeatherCondition;
 
     constructor (apiData) {
         const {location, current, forecast} = apiData;
-        this.#location = location;
-        this.#current = current;
-        this.#forecast = forecast;
+        this.#extractLocation(location);
+        this.#extractCurrentWeatherCondition(current);
+        this.#extractForecastWeatherCondition(forecast);
+    }
+
+    #extractLocation (location) {
+        const {name, region, country, localtime} = location;
+        this.#location = {
+            name,
+            region,
+            country,
+            localtime
+        };
+    }
+
+    #extractCurrentWeatherCondition (current) {
+        const {last_updated, temp_c, feelslike_c, wind_kph, wind_dir, humidity} = current;
+        const {text, icon} = current.condition;
+        const {co, no2, o3, so2} = current.air_quality;
+        const us_epa_index = current.air_quality['us-epa-index'];
+
+        this.#currentWeatherCondition = {
+            lastUpdatedAt: last_updated,
+            tempC: temp_c,
+            feelslikeC: feelslike_c,
+            text: text,
+            icon: icon,
+            windKph: wind_kph,
+            windDir: wind_dir,
+            humidity: humidity,
+            co: co,
+            no2: no2,
+            o3: o3,
+            so2: so2,
+            usEpaIndex: us_epa_index
+        };
+    }
+
+    #extractForecastWeatherCondition (forecast) {
 
     }
 
@@ -15,11 +51,11 @@ export class WeatherCondition {
         return this.#location;
     }
 
-    getCurrent () {
-        return this.#current;
+    getCurrentWeatherCondition () {
+        return this.#currentWeatherCondition;
     }
 
-    getForecast () {
-        return this.#forecast;
+    getForecastWeatherCondition () {
+        return this.#forecastWeatherCondition;
     }
 }
